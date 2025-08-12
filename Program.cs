1ec364fbcde76e943927f487ed181fb385575c29
Octavio Henrique Knupp Lucio <octavioknupp882@gmail.com>
2025-08-12 20:31:37 -0300

    Aula 2
    Aprendemos sobre variável e concatenação

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hello Word!!");
+//nome usar a variavel do tipo string
+string nomeAluno = "Octavio Henrique Knupp Lucio";
+// numero usa a variavel do tipo int
+int periodo = 1;
+//nota usa a varivel do tipo float
+float nota1Bim = 9.3f;
+//nota usa a varivel do tipo float
+float nota2Bim = 7.5f;
+//nota usa a varivel do tipo float
+float somaNotas = nota1Bim + nota2Bim;
+//nota usa a varivel do tipo float
+float media = somaNotas / 2;
+Console.WriteLine("Nome: "+nomeAluno);
+Console.WriteLine("Periodo: "+periodo);
+Console.WriteLine("Nota 1 Bim: "+nota1Bim);
+Console.WriteLine("Nota 2 Bim: "+nota2Bim);
+Console.WriteLine("Soma das notas: "+somaNotas);
+Console.WriteLine("Média das notas: "+media);
+Console.ReadKey();
+
+//próxima aula 
